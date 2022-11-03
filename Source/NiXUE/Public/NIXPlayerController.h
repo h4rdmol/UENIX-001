e932be46f666d9dc978fe928f1fab16902d8901e
rotermel.valera <valeriy-rotermel@Valeriys-MacBook-Pro.local>
2022-11-04 01:35:33 +0200

    Initial commint

---
+++ Source/NiXUE/Public/NIXPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "NIXPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NIXUE_API ANIXPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
